51b156e6c38168e27879e73c025ddd3596343f2e
Vizonex <VizonexBusiness@gmail.com>
2026-03-19 11:50:48 -0500

    fix more formatting problems with test_process

--- tests/test_process.py
+++ tests/test_process.py
@@ -880,7 +880,8 @@ class _AsyncioTests:
         async def copy_stdin_to_stdout(stdin):
             # See https://github.com/MagicStack/uvloop/issues/363
             # A program that copies stdin to stdout character by character
-            code = "import sys, shutil\nshutil.copyfileobj(sys.stdin, sys.stdout, 1)"
+            code = "import sys, shutil\n"
+            code += "shutil.copyfileobj(sys.stdin, sys.stdout, 1)"
             proc = await asyncio.create_subprocess_exec(
                 sys.executable,
                 b"-W",
