738e93e9425677a49b0e3558c6fca0b29c426472
Vizonex <VizonexBusiness@gmail.com>
2026-03-19 13:09:30 -0500

    reformat with black once more

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -469,7 +469,6 @@ print(fd0 == fd1, flush=True)
 
         self.loop.run_until_complete(runner())
 
-
     def test_signals_fork_in_thread(self):
         if (
             sys.platform == "win32"
@@ -478,9 +477,7 @@ print(fd0 == fd1, flush=True)
             raise unittest.SkipTest(
                 "no add_signal_handler on asyncio loop on Windows"
             )
-        if (
-            sys.platform == "darwin"
-        ):
+        if sys.platform == "darwin":
             raise unittest.SkipTest(
                 "signal_handler is having problems on apple currently."
             )
