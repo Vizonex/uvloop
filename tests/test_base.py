1f9ff0ab7646c7f209319567afedab8fad5f88b4
Vizonex <VizonexBusiness@gmail.com>
2026-03-20 13:13:49 -0500

    uvloop still has the same problem on windows 3.11

--- tests/test_base.py
+++ tests/test_base.py
@@ -173,7 +173,7 @@ class _TestBase:
     def test_call_later_2(self):
         # Test that loop.call_later triggers an update of
         # libuv cached time.
-        if self.implementation == "asyncio" and sys.version_info == (3, 11):
+        if sys.version_info == (3, 11):
             if sys.platform == "win32":
                 raise unittest.SkipTest(
                     "rounding errors are still present in 3.11"
